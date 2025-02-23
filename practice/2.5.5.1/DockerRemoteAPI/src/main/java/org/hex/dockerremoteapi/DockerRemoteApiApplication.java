d4d0a8b07957a8daede19e31bdf9de7b6a66c21e
HEX <123macanic@naver.com>
2025-02-23 20:28:30 +0900

    2.5.5.1 DockerRemoteAPI - Java Project init

---
+++ practice/2.5.5.1/DockerRemoteAPI/src/main/java/org/hex/dockerremoteapi/DockerRemoteApiApplication.java
@@ -0,0 +1,13 @@
+package org.hex.dockerremoteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRemoteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerRemoteApiApplication.class, args);
+    }
+
+}
---
+++ practice/2.5.5.1/DockerRemoteAPI/src/test/java/org/hex/dockerremoteapi/DockerRemoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hex.dockerremoteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerRemoteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
