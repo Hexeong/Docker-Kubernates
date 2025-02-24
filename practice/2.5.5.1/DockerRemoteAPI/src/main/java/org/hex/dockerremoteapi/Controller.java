aaba04d86f6b3c2ab046ef68bbe820d6afebf3f6
HEX <123macanic@naver.com>
2025-02-24 16:44:08 +0900

    practice: [Feature]자바 라이브러리를 통한 도커 이미지 리스트 출력하기 기능  구현

--- practice/2.5.5.1/DockerRemoteAPI/src/main/java/org/hex/dockerremoteapi/Controller.java
+++ practice/2.5.5.1/DockerRemoteAPI/src/main/java/org/hex/dockerremoteapi/Controller.java
@@ -1,2 +1,27 @@
-package org.hex.dockerremoteapi;public class Controller {
+package org.hex.dockerremoteapi;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.ListImagesCmd;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    private final DockerClient dockerClient;
+
+    public Controller(DockerClient dockerClient) {
+        this.dockerClient = dockerClient;
+    }
+
+    @GetMapping("/docker/image/list/all")
+    public ResponseEntity<?> accessTest() {
+
+        ListImagesCmd imageList = dockerClient.listImagesCmd();
+
+        System.out.println(imageList.exec());;
+
+        return ResponseEntity.ok().build();
+    }
 }
--- practice/2.5.5.1/DockerRemoteAPI/src/main/java/org/hex/dockerremoteapi/config/DockerClientConfig.java
+++ practice/2.5.5.1/DockerRemoteAPI/src/main/java/org/hex/dockerremoteapi/config/DockerClientConfig.java
@@ -1,2 +1,34 @@
-package org.hex.dockerremoteapi.config;public class DockerClientConfig {
+package org.hex.dockerremoteapi.config;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DockerClientConfig {
+
+    // Windows Named Pipe 경로 (Docker Desktop에서 사용 가능)
+    private static final String DOCKER_NPIPE = "npipe:////./pipe/docker_engine";
+
+    // localhost:2375 연결 (Docker Desktop에서 TLS 없이 연결하는 경우)
+    private static final String DOCKER_LOCALHOST_WITHOUT_TLS = "tcp://localhost:2375";
+
+    @Bean
+    public DockerClient dockerClient() {
+        // Docker 클라이언트 구성
+        DefaultDockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(DOCKER_LOCALHOST_WITHOUT_TLS)  // 필요에 따라 DOCKER_NPIPE로 변경 가능
+                .build();
+
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .build();
+
+        return DockerClientImpl.getInstance(config, httpClient);
+    }
 }
